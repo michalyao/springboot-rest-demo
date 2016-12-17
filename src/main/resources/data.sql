7302d4a4f80ed697f916ea2bba2dedc0ac6d0bb8
michalyao <yaoyao0777@gmail.com>
2016-12-17 18:13:23 +0800

    create: 创建H2 mock数据脚本

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO todo (title, completed, `orders`, url) VALUES ('看一本书', FALSE, 1, 'readbook');
+INSERT INTO todo (title, completed, `orders`, url) VALUES ('写一行代码', FALSE, 2, 'coding');
+INSERT INTO todo (title, completed, `orders`, url) VALUES ('吃一顿饭', FALSE, 3, 'food');
